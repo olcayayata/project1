891d31659de8f309d6b5070baa2ea77f27ee4d07
olcayayata <olcayayata4@gmail.com>
2021-08-02 15:27:10 -0400

    my changes

--- src/com/codewitholcay/Main.java
+++ src/com/codewitholcay/Main.java
@@ -8,8 +8,6 @@ import java.util.Locale;
 public class Main {
 
 	public static void main(String[] args) {
-		final float pt = 3.14F;
-		//pi = 1;
-		//System.out.println(Arrays.deepToString(numbers));
+
 	}
 }
