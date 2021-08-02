307816b92ab6b708dad43a9bc1732bd9e8c2ee9b
olcayayata <olcayayata4@gmail.com>
2021-08-02 15:12:46 -0400

    first commit

---
+++ src/com/codewitholcay/Main.java
@@ -0,0 +1,15 @@
+package com.codewitholcay;
+
+import java.awt.*;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Locale;
+
+public class Main {
+
+	public static void main(String[] args) {
+		final float pt = 3.14F;
+		//pi = 1;
+		//System.out.println(Arrays.deepToString(numbers));
+	}
+}
